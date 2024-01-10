b5b4a8353530179c252ee09277b6bf363125213e
Ahmed yussuf Abdullahi <ahmedyussuf917@gmail.com>
2024-01-10 18:10:28 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+# app/app.py
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string_param>')
+def print_string(string_param):
+    print(string_param)
+    return f'<p>String: {string_param}</p>'
+
+@app.route('/count/<int_param>')
+def count(int_param):
+    numbers = '\n'.join(str(i) for i in range(int_param + 1))
+    return f'<pre>{numbers}</pre>'
+
+@app.route('/math/<float:num1><operation><float:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+
+    return f'<p>Result: {result}</p>'
+
